d8dd14a4f4b3879ec32b9826418b94e3af5e27db
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-07-16 15:44:40 +0530

    Generate cookies

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/auth.js";
 import usersRouter from "./routes/users.js";
@@ -24,6 +25,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 // Middleware
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);

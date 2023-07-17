4db0bcee7757ea804d56a30e521a6cde2c8bd10f
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-07-17 19:14:17 +0530

    add countByCity

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -5,6 +5,7 @@ import {
 	deleteHotel,
 	getHotel,
 	getHotels,
+	countByCity,
 } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -20,9 +21,11 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 //GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 
 export default router;

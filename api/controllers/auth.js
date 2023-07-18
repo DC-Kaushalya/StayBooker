a6badf9fd2e9e215d3d2cc184385ee29569ab89b
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-07-18 15:24:45 +0530

    add new client

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,8 @@
-import User from "../models/User.js";
-import bcrypt from "bcryptjs";
-import { createError } from "./../utils/error.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+import User from "../models/User.js";
+import { createError } from "./../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {

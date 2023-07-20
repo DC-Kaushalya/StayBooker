0249fa90e0d0b3329f49143c625e22a54102d3e8
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-07-20 13:13:32 +0530

    Update search (price and nights)

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import { SearchContextProvider } from "./context/SearchContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<SearchContextProvider>
+			<App />
+		</SearchContextProvider>
+	</React.StrictMode>
 );
--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -11,7 +11,7 @@ import useFetch from "../../hooks/useFetch";
 const List = () => {
 	const location = useLocation();
 	const [destination, setDestination] = useState(location.state.destination);
-	const [date, setDate] = useState(location.state.date);
+	const [dates, setDates] = useState(location.state.dates);
 	const [openDate, setOpenDate] = useState(false);
 	const [options, setOptions] = useState(location.state.options);
 	const [min, setMin] = useState(undefined);
@@ -42,19 +42,19 @@ const List = () => {
 							<span
 								onClick={() => setOpenDate(!openDate)}
 							>{`${format(
-								date[0].startDate,
+								dates[0].startDate,
 								"MM/dd/yyyy"
 							)} to ${format(
-								date[0].endDate,
+								dates[0].endDate,
 								"MM/dd/yyyy"
 							)}`}</span>
 							{openDate && (
 								<DateRange
 									onChange={(item) =>
-										setDate([item.selection])
+										setDates([item.selection])
 									}
 									minDate={new Date()}
-									ranges={date}
+									ranges={dates}
 								/>
 							)}
 						</div>

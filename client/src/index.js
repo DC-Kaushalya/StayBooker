66726949806a51c7f196e41de5f7c849b77ec6a6
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-07-20 23:23:18 +0530

    Create login section

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import { SearchContextProvider } from "./context/SearchContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<SearchContextProvider>
-			<App />
-		</SearchContextProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
 );

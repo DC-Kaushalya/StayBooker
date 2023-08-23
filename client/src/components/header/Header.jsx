2a77fb36bd985e0d7b1a148f2061a72a1ea456f6
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-08-23 18:09:20 +0530

    Content modify

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -89,7 +89,7 @@ const Header = ({ type }) => {
             <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
             <p className="headerDesc">
               Get rewarded for your travels – unlock instant savings of 10% or more with a free
-              Lamabooking account
+              StayBooker account
             </p>
             {!user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
